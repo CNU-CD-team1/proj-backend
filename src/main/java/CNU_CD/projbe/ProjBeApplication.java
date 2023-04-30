ae1afbff6ee165c932993edbb5877b072fca33c7
kwonhyunjun <kwonhyunjun7@gmail.com>
2023-04-30 10:59:56 +0900

    project init

---
+++ src/main/java/CNU_CD/projbe/ProjBeApplication.java
@@ -0,0 +1,13 @@
+package CNU_CD.projbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CNU_CD/projbe/ProjBeApplicationTests.java
@@ -0,0 +1,13 @@
+package CNU_CD.projbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
